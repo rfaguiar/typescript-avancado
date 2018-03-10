4d4d54e6df25060a033e80e91b045ec6d156cc0a
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-03-10 13:46:47 -0300

    Curso de TypeScript Avançado

---
+++ README.m
@@ -0,0 +1,1 @@
+# typescript-avançado
---
+++ alurabank/app/js/controllers/NegociacaoController.js
@@ -0,0 +1,112 @@
+System.register(["../views/index", "../models/index", "../helpers/decorators/index", "../services/index", "../helpers/index"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+            function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+            function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+            step((generator = generator.apply(thisArg, _arguments || [])).next());
+        });
+    };
+    var __moduleName = context_1 && context_1.id;
+    var index_1, index_2, index_3, index_4, index_5, NegociacaoController, DiaDaSemana;
+    return {
+        setters: [
+            function (index_1_1) {
+                index_1 = index_1_1;
+            },
+            function (index_2_1) {
+                index_2 = index_2_1;
+            },
+            function (index_3_1) {
+                index_3 = index_3_1;
+            },
+            function (index_4_1) {
+                index_4 = index_4_1;
+            },
+            function (index_5_1) {
+                index_5 = index_5_1;
+            }
+        ],
+        execute: function () {
+            NegociacaoController = class NegociacaoController {
+                constructor() {
+                    this._negociacoes = new index_2.Negociacoes();
+                    this._negociacoesView = new index_1.NegociacoesView('#negociacoesView');
+                    this._mensagemView = new index_1.MensagemView('#mensagemView');
+                    this._service = new index_4.NegociacaoService();
+                    this._negociacoesView.update(this._negociacoes);
+                }
+                importaDados() {
+                    return __awaiter(this, void 0, void 0, function* () {
+                        try {
+                            const negociacoesParaImportar = yield this._service
+                                .obterNegociacoes(res => {
+                                if (res.ok) {
+                                    return res;
+                                }
+                                else {
+                                    throw new Error(res.statusText);
+                                }
+                            });
+                            const negociacoesJaImportadas = this._negociacoes.paraArray();
+                            negociacoesParaImportar
+                                .filter(negociacao => !negociacoesJaImportadas.some(jaImportada => negociacao.ehIgual(jaImportada)))
+                                .forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                            this._negociacoesView.update(this._negociacoes);
+                        }
+                        catch (err) {
+                            this._mensagemView.update(err.message);
+                        }
+                    });
+                }
+                adiciona() {
+                    let data = new Date(this._inputData.val().replace(/-/g, ','));
+                    if (!this._ehDiaUtil(data)) {
+                        this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+                        return;
+                    }
+                    const negociacao = new index_2.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    this._negociacoes.adiciona(negociacao);
+                    index_5.imprime(negociacao, this._negociacoes);
+                    this._negociacoesView.update(this._negociacoes);
+                    this._mensagemView.update('Negociação adicionada com sucesso!');
+                }
+                _ehDiaUtil(data) {
+                    return data.getDay() != DiaDaSemana.SABADO && data.getDay() != DiaDaSemana.DOMINGO;
+                }
+            };
+            __decorate([
+                index_3.domInject('#data')
+            ], NegociacaoController.prototype, "_inputData", void 0);
+            __decorate([
+                index_3.domInject('#quantidade')
+            ], NegociacaoController.prototype, "_inputQuantidade", void 0);
+            __decorate([
+                index_3.domInject('#valor')
+            ], NegociacaoController.prototype, "_inputValor", void 0);
+            __decorate([
+                index_3.throttle()
+            ], NegociacaoController.prototype, "importaDados", null);
+            __decorate([
+                index_3.throttle()
+            ], NegociacaoController.prototype, "adiciona", null);
+            exports_1("NegociacaoController", NegociacaoController);
+            (function (DiaDaSemana) {
+                DiaDaSemana[DiaDaSemana["DOMINGO"] = 0] = "DOMINGO";
+                DiaDaSemana[DiaDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+                DiaDaSemana[DiaDaSemana["TERCA"] = 2] = "TERCA";
+                DiaDaSemana[DiaDaSemana["QUARTA"] = 3] = "QUARTA";
+                DiaDaSemana[DiaDaSemana["QUINTA"] = 4] = "QUINTA";
+                DiaDaSemana[DiaDaSemana["SEXTA"] = 5] = "SEXTA";
+                DiaDaSemana[DiaDaSemana["SABADO"] = 6] = "SABADO";
+            })(DiaDaSemana || (DiaDaSemana = {}));
+        }
+    };
+});
